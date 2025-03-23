a88203c50db4145bacb39794678e020f14b5ae72
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2025-03-22 20:58:29 -0600

    Uncomment route handlers for games, users, and copies in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 // Require controller modules.
 
-//router.use("/games", require("./games"));
+router.use("/games", require("./games"));
 router.use("/consoles", require("./consoles"));
-//router.use("/users", require("./users"));
+router.use("/users", require("./users"));
 //router.use("/copies", require("./copies"));
 
 module.exports = router;
